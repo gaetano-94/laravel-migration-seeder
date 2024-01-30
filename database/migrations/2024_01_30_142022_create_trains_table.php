215598fbe909e98928c3cb9e4f03274b2a42fa7e
gaetano-94 <gaetano.panico1994@gmail.com>
2024-01-30 17:02:21 +0100

    feat: creazione dati dei train

--- database/migrations/2024_01_30_142022_create_trains_table.php
+++ database/migrations/2024_01_30_142022_create_trains_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+
+            $table->string('company', 50);
+            $table->string('free_on_rail', 50);
+            $table->string('arrival_station', 50);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code', 100);
+            $table->tinyInteger('number_of_carriages')->unsigned();
+            $table->time('on_time')->nullable();
+            $table->string('cancelled', 50)->nullable();
+
             $table->timestamps();
         });
     }
